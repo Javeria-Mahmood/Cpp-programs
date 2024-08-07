4942855c1629f57d5852d71b4101c5565e877037
Javeria-Mahmood <jazz200517@gmail.com>
2024-08-07 21:59:34 +0500

    Continuous Hearts
    Continuous Hearts strip using C++
    Concepts of loops, colors and basic coding included

---
+++ heart.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+// ANSI escape codes for text colors
+#define RED     "\033[31m"      /* Red */
+int main() 
+{
+	while(true)
+	{
+    cout<<RED<<"   ****	    ****"<<endl;
+    cout<<RED<<" *******   *******"<<endl;
+    cout<<RED<<"********* *********"<<endl;
+    cout<<RED<<"*******************"<<endl;
+    cout<<RED<<" *****************"<<endl;
+    cout<<RED<<"  ***************"<<endl;
+    cout<<RED<<"   *************"<<endl;
+    cout<<RED<<"    ***********"<<endl;
+    cout<<RED<<"     *********"<<endl;
+    cout<<RED<<"       *****"<<endl;
+    cout<<RED<<"        ***"<<endl;
+    cout<<RED<<"         *"<<endl;
+    cout<<"                     "<<endl;
+    cout<<"                      "<<endl;
+}
+    return 0;
+}
